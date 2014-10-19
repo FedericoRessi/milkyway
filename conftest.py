4824642a667915957ef1397763a1ed5d229c5a35
Federico Ressi <federico.ressi@gmail.com>
2014-10-19 11:06:34 +0200

    Remove pytest-knowns

--- conftest.py
+++ conftest.py
@@ -13,31 +13,6 @@
 '''
 
 import logging
-import os
-import shelve
-
-import ptknows
 
 
 logger = logging.getLogger(__name__)
-
-
-store = shelve.open(os.path.join(os.curdir, 'ptknows.dbm'), writeback=True)
-
-
-def open_store():
-    '''
-    Open store for ptknownw
-    '''
-    return store
-
-
-def close_store():
-    '''
-    Close and save store for ptknownw
-    '''
-    store.sync()
-    store.close()
-
-ptknows.register_store(open_store, close_store)
-pytest_plugins = ["ptknows", ]
